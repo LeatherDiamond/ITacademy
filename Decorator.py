bde405cac2e0c93fd13f544ffab49b84a990cc8f
Aliaksandr <alexander.dovguchits@gmail.com>
2022-09-17 18:21:02 +0300

    Ready decorator

--- Decorator.py
+++ Decorator.py
@@ -1,2 +1,17 @@
+#Login request.
 input = str(input("Insert your login:"))
-print(input)
+#Decorator.
+def account_balance(func):
+    def wrapper_decorator(*args, **kwargs):
+        if input != 'admin':
+            return'Access denied!'
+        value = func(*args, **kwargs)
+        return value
+    return wrapper_decorator
+#Applying decorator to the main function.
+@account_balance
+def balance():
+    return'Your account balance is 5789 USD.'
+#Launching the function with decorator.
+funct_launch = balance()
+print(funct_launch)
