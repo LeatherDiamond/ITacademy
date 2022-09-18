b06451e85533a3b7dc0a6c62e9556a6145d64fbb
Aliaksandr <alexander.dovguchits@gmail.com>
2022-09-18 19:50:53 +0300

    Ready version 2.0 with some additions

--- Robot's route calculation.py
+++ Robot's route calculation.py
@@ -1,8 +1,8 @@
 #Request of necessary data from user and notification.
 print("Welcom to your robot's control menu. Here you can insert direction and quantity of steps for your robot.\
- Length of one step is 100 cm. Robot can move up, down, left and right. To insert direction and quantity of steps\
+ Length of one step is 1m. Robot can move up, down, left and right. To insert direction and quantity of steps\
  please use a short comand:'u', 'd', 'l' or 'r' and after space insert quantity of steps ('u', 'd', 'l', or 'r' ->\
- space -> q-ty of steps). To cancel the insert field please push 'enter' or insert 'q' and push 'enter'.")
+ space -> q-ty of steps). To cancel the insert field please insert 'q' and push 'enter'.")
 steps = {
     'u':0,
     'd':0,
@@ -11,17 +11,26 @@ steps = {
 }
 while True:
     data_request = input("Please insert direction and quantity of steps using space button:")
+# Splitting input data and ending the cycle with "q".
     try:
         data_request_list = data_request.split()
-        if data_request_list[0] == 'q' or data_request_list[0] == '':
+        if data_request_list[0] == 'q':
             break
+# Checking value of steps (if it's valid - positive number) and ending the cycle if value is not valid.
+        elif int(data_request_list[1]) < 0:
+            print("Quantity of steps must be a positive number!")
+            break
+# Making the list with input data.
         command = data_request_list[0]
         command_value = int(data_request_list[-1])
         steps[command] = steps[command] + command_value
+#Printing the message if input data is not valid. 
     except:
-        print("Please insert data as it is discribed at the very beggining. ('u', 'd', 'l', or 'r' -> space -> q-ty of steps)")
+        print("Please insert data as it is discribed at the very beggining. ('u', 'd', 'l', or 'r' -> space -> q-ty of steps)")\
 # Calculation of robot's route.
 import math
-length_route = math.sqrt((steps['u']**2 - steps['d']**2) + (steps['r']**2 - steps['l']**2))
+from math import fabs
+# Adding module to calculation for cases when routes "down" and "left" are bigger than "up" and "right" or when "up" and "right" are zero.
+length_route = math.sqrt(fabs((steps['u']**2 - steps['d']**2)) + fabs((steps['r']**2 - steps['l']**2)))
 length_route_round = round(length_route)
 print(f"Your robot's route is {length_route_round} meters.")
