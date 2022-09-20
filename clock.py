d1fcee0dcb1def690de0f56a94d5822d5f6cf3df
Aliaksandr <alexander.dovguchits@gmail.com>
2022-09-20 20:53:42 +0300

    Watch in progress

--- clock.py
+++ clock.py
@@ -1,2 +1,74 @@
-x = 'asdasd'
-print(x)
+#Import of necessary libraries.
+import datetime
+import time
+import os
+#Drawing of numbers for clock.
+zero = """
+⬛⬛⬛⬛
+⬛    ⬛
+⬛    ⬛
+⬛    ⬛
+⬛⬛⬛⬛"""
+one = """
+⬛⬛
+  ⬛
+  ⬛
+  ⬛
+⬛⬛⬛"""
+two = """
+⬛⬛⬛⬛
+      ⬛
+⬛⬛⬛⬛
+⬛
+⬛⬛⬛⬛"""
+three = """
+⬛⬛⬛⬛
+      ⬛
+⬛⬛⬛⬛
+      ⬛
+⬛⬛⬛⬛"""
+four = """
+⬛    ⬛
+⬛    ⬛
+⬛⬛⬛⬛
+      ⬛
+      ⬛"""
+five = """
+⬛⬛⬛⬛
+⬛
+⬛⬛⬛⬛
+      ⬛
+⬛⬛⬛⬛"""
+six = """
+⬛⬛⬛⬛
+⬛
+⬛⬛⬛⬛
+⬛    ⬛
+⬛⬛⬛⬛"""
+seven = """
+⬛⬛⬛⬛
+      ⬛
+      ⬛
+      ⬛
+      ⬛"""
+eight = """
+⬛⬛⬛⬛
+⬛    ⬛
+⬛⬛⬛⬛
+⬛    ⬛
+⬛⬛⬛⬛"""
+nine = """
+⬛⬛⬛⬛
+⬛    ⬛
+⬛⬛⬛⬛
+      ⬛
+⬛⬛⬛⬛"""
+while True:
+#Import of current time.
+    cdt = datetime.datetime.now()
+#Transformation of current date and time to necessary format (Hours, Minutes and Seconds).
+    cdt = cdt.strftime("%H:%M:%S")
+    time.sleep(0.2)
+#Clearing the console.
+    os.system('cls') 
+    print(cdt)
