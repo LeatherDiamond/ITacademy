9dbea65bff0541a2f0f0968c118aaf1af84e9803
Aliaksandr <alexander.dovguchits@gmail.com>
2022-09-22 22:45:06 +0300

    Ready cloack. Version 3.0 updated

--- clock.py
+++ clock.py
@@ -72,69 +72,79 @@ time_numbers = {
   
   """
 }
-#Function to create lists with accordance to current data and adding to the list that will be made by drawed before numbers.
+
+
 def format_share_time(time_str):
+#Function to create lists with accordance to current data and adding to the list that will be made by drawed before numbers.'''
     output_numbers = []
     conver_to_list = []
     try:
         for i in time_str:
             output_numbers.append(time_numbers[i])
     except:
-        print('Error!')
+        print('Fuck')
     for i in output_numbers:
         conver_to_list.append(i.split('\n'))
     return conver_to_list
 
-#Function to replace emoty separator from dictionary to black squares.
+
 def replace_string(string_for_replace):
+#Function to replace emoty separator from dictionary to black squares.
     result = string_for_replace
     result[2] = '⬛'
     result[5] = '⬛'
     return result
-#Making a circle with time import and creation of a list with time data ready for print.
-while True:
+
+
+def unpack(s):
+#Function to unpack printed strings in the end.
+    return " ".join(map(str, s))
+
+
+
+def check_time():
+#Function to insert current time from the system.
     cdt = datetime.now()
     cdt = cdt.strftime("%H:%M:%S")
     list_time = format_share_time(cdt)
+    last_result = []
     for i in list_time:
         result = []
         for j in range(len(i)):
             result.append(list(map(itemgetter(j), list_time)))
-        def unpack(s):
-            return " ".join(map(str, s))
         clean_result = result[1:]
-#Printing separator down->top.
-        os.system('cls')
-        print(f'{unpack(clean_result[0])}\n{unpack(clean_result[1])}\n{unpack(clean_result[2])}\n{unpack(clean_result[3])}\n{unpack(replace_string(clean_result[4]))}\n')
-        time.sleep(0.2)
-        os.system('cls')
-        print(f'{unpack(clean_result[0])}\n{unpack(clean_result[1])}\n{unpack(clean_result[2])}\n{unpack(replace_string(clean_result[3]))}\n{unpack(clean_result[4])}\n')
-        time.sleep(0.2)
-        os.system('cls')
-        print(f'{unpack(clean_result[0])}\n{unpack(clean_result[1])}\n{unpack(replace_string(clean_result[2]))}\n{unpack(clean_result[3])}\n{unpack(clean_result[4])}\n')
-        time.sleep(0.2)
-        os.system('cls')
-        print(f'{unpack(clean_result[0])}\n{unpack(replace_string(clean_result[1]))}\n{unpack(clean_result[2])}\n{unpack(clean_result[3])}\n{unpack(clean_result[4])}\n')
-        time.sleep(0.2)
-        os.system('cls')
-        print(f'{unpack(replace_string(clean_result[0]))}\n{unpack(clean_result[1])}\n{unpack(clean_result[2])}\n{unpack(clean_result[3])}\n{unpack(clean_result[4])}\n')
-        time.sleep(0.2)
-        os.system('cls')
-#Printing separator up->down.
-        print(f'{unpack(replace_string(clean_result[0]))}\n{unpack(clean_result[1])}\n{unpack(clean_result[2])}\n{unpack(clean_result[3])}\n{unpack(clean_result[4])}\n')
-        time.sleep(0.2)
-        os.system('cls')
-        print(f'{unpack(clean_result[0])}\n{unpack(replace_string(clean_result[1]))}\n{unpack(clean_result[2])}\n{unpack(clean_result[3])}\n{unpack(clean_result[4])}\n')
-        time.sleep(0.2)
-        os.system('cls')
-        print(f'{unpack(clean_result[0])}\n{unpack(clean_result[1])}\n{unpack(replace_string(clean_result[2]))}\n{unpack(clean_result[3])}\n{unpack(clean_result[4])}\n')
-        time.sleep(0.2)
-        os.system('cls')
-        print(f'{unpack(clean_result[0])}\n{unpack(clean_result[1])}\n{unpack(clean_result[2])}\n{unpack(replace_string(clean_result[3]))}\n{unpack(clean_result[4])}\n')
-        time.sleep(0.2)
-        os.system('cls')
-        print(f'{unpack(clean_result[0])}\n{unpack(clean_result[1])}\n{unpack(clean_result[2])}\n{unpack(clean_result[3])}\n{unpack(replace_string(clean_result[4]))}\n')
-        time.sleep(0.2)
-        os.system('cls')
+
+        last_result += clean_result
         break
+
+    return last_result
+
+
+while True:
+#Cicle to print time with drawed numbers and moving separator.
+    clean_result = check_time()
+    os.system('cls')
+    print(f'{unpack(check_time()[0])}\n{unpack(check_time()[1])}\n{unpack(check_time()[2])}\n{unpack(check_time()[3])}\n{unpack(replace_string(check_time()[4]))}\n')
+    time.sleep(0.2)
+    os.system('cls')
+    print(f'{unpack(check_time()[0])}\n{unpack(check_time()[1])}\n{unpack(check_time()[2])}\n{unpack(replace_string(check_time()[3]))}\n{unpack(check_time()[4])}\n',)
+    time.sleep(0.2)
+    os.system('cls')
+    print(f'{unpack(check_time()[0])}\n{unpack(check_time()[1])}\n{unpack(replace_string(check_time()[2]))}\n{unpack(check_time()[3])}\n{unpack(check_time()[4])}\n',)
+    time.sleep(0.2)
+    os.system('cls')
+    print(f'{unpack(check_time()[0])}\n{unpack(replace_string(check_time()[1]))}\n{unpack(check_time()[2])}\n{unpack(check_time()[3])}\n{unpack(check_time()[4])}\n',)
+    time.sleep(0.2)
+    os.system('cls')
+    print(f'{unpack(replace_string(check_time()[0]))}\n{unpack(check_time()[1])}\n{unpack(check_time()[2])}\n{unpack(check_time()[3])}\n{unpack(check_time()[4])}\n')
+    time.sleep(0.2)
+    os.system('cls')
+    print(f'{unpack(check_time()[0])}\n{unpack(replace_string(check_time()[1]))}\n{unpack(check_time()[2])}\n{unpack(check_time()[3])}\n{unpack(check_time()[4])}\n')
+    time.sleep(0.2)
+    os.system('cls')
+    print(f'{unpack(check_time()[0])}\n{unpack(check_time()[1])}\n{unpack(replace_string(check_time()[2]))}\n{unpack(check_time()[3])}\n{unpack(check_time()[4])}\n')
+    time.sleep(0.2)
+    os.system('cls')
+    print(f'{unpack(check_time()[0])}\n{unpack(check_time()[1])}\n{unpack(check_time()[2])}\n{unpack(replace_string(check_time()[3]))}\n{unpack(check_time()[4])}\n')
+    time.sleep(0.2)
     
