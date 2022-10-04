8a4024ed4d0f87e9213d328b4b1e2539538ad642
Aliaksandr <alexander.dovguchits@gmail.com>
2022-10-05 00:31:09 +0300

    Allowed hosts added

--- src/proj/settings.py
+++ src/proj/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = local_settings.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['AlexanderDovguchits.pythonanywhere.com']
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['alexanderdovguchits.pythonanywhere.com', '127.0.0.1']
+
 
 
 # Application definition
