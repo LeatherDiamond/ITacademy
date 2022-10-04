f9b3bf28e19f1cb09d7b60fdff3c609e67fbad6e
Aliaksandr <alexander.dovguchits@gmail.com>
2022-10-04 22:43:14 +0300

    ready for cloning project

--- src/proj/settings.py
+++ src/proj/settings.py
@@ -111,8 +111,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = '/home/AlexanderDovguchits/static'
-STATIC_MEDIA = '/home/AlexanderDovguchits/media'
+STATIC_ROOT = '/home/AlexanderDovguchits/ITacademy/static'
+STATIC_MEDIA = '/home/AlexanderDovguchits/ITacademy/media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
