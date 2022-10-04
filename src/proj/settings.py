52f8fcabc9bc0fdd56584b814183e63481ff54ae
Aliaksandr <alexander.dovguchits@gmail.com>
2022-10-04 20:17:44 +0300

    Created project first step

--- src/proj/settings.py
+++ src/proj/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+from . import local_settings
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-ofk-g#t$wqm1jvk%@3l60v+_5mzuuw)a!0&5tafgt8dbvd3u3&"
+SECRET_KEY = local_settings.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['AlexanderDovguchits.pythonanywhere.com']
 
 
 # Application definition
@@ -110,6 +110,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = '/home/AlexanderDovguchits/static'
+STATIC_MEDIA = '/home/AlexanderDovguchits/media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
