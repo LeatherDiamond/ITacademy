e96e7943af7402ad4a22cb7981a58746b7c74b67
Aliaksandr <alexander.dovguchits@gmail.com>
2022-11-08 22:36:50 +0300

    comment form added

--- src/catalog/models.py
+++ src/catalog/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.utils import timezone
+from product_card.models import Book
+from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractBaseUser, UserManager, PermissionsMixin
 
 # Create your models here.
+
+class Comment(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE,
+                             related_name='comments')
+    username = models.ForeignKey('AppUser', on_delete=models.CASCADE,
+                                 related_name='user_name')
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        ordering = ['-created_date']
+
+        def __str__(self):
+            return self.text
+
+
+class AppUser(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(
+        unique=True,
+        max_length=30,
+    )
+    email = models.EmailField(blank=True, null=True)
+
+    is_staff = models.BooleanField(
+        default=False,
+    )
+    is_active = models.BooleanField(
+        default=True,
+    )
+    is_superuser = models.BooleanField(
+        default=True
+    )
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'username'
+
+    class Meta:
+        verbose_name = 'username'
+        verbose_name_plural = 'usernames'
+        abstract = False
--- src/catalog/views.py
+++ src/catalog/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.views import generic, View
 from product_card.models import Book
 from django.core.paginator import Paginator
+from .forms import CommentForm
+from django.shortcuts import get_object_or_404, render
+from .models import Comment
+from django.http import HttpResponse, HttpResponseRedirect
 
 # Create your views here.
 
@@ -25,5 +29,32 @@ class CatalogView(View):
 
 
 class BookDetail(generic.DetailView):
-    template_name = 'catalog/book_detail.html'
-    model = Book
+    # template_name = 'catalog/book_detail.html'
+    # model = Book
+    # comment_form = CommentForm(request.POST)
+
+    def get(self, request, *args, **kwargs):
+        book = get_object_or_404(Book)
+        last_posts = Book.objects.all().order_by('-id')[:3]
+        comment_form = CommentForm()
+        return render(request, 'catalog/book_detail.html', context={
+            'book': book,
+            'last_posts': last_posts,
+            'comment_form': comment_form,
+        })    
+
+    
+    def post(self, request, *args, **kwargs):
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            text = request.POST['text']
+            username = self.request.user
+            book = get_object_or_404(Book)
+            comment = Comment.objects.create(book=book, username=username,
+                                             text=text)
+            return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
+        return render(request, 'catalog/book_detail.html', context={
+            'comment_form': comment_form
+        })
+
+    
