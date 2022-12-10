396a42c3a6c6db0a9091c57a9be6cab93954e2fe
Aliaksandr <alexander.dovguchits@gmail.com>
2022-12-10 17:08:16 +0300

    vers. 3.0

--- src/proj/settings.py
+++ src/proj/settings.py
@@ -153,3 +153,16 @@ AUTHENTICATION_BACKENDS = (
 
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '1018402011643-42fntmtfp7oe9j6vdepo1s3hio1frvh6.apps.googleusercontent.com'
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = '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'
+
+SOCIAL_AUTH_PIPELINE = (
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'pd.views.Users.save_profile', # <--- set the path to the function
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+)
